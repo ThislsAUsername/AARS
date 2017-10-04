d064d46e6a4c88fa70a3c239d4fa40e496d2674e
Name <Visage@asu.edu>
2017-10-04 01:34:15 -0700

    Register file cleanup. Removes HI and LO.

--- mars/venus/RegistersWindow.java
+++ mars/venus/RegistersWindow.java
@@ -82,7 +82,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
    	
        public Object[][] setupWindow(){
          int valueBase = NumberDisplayBaseChooser.getBase(settings.getDisplayValuesInHex());
-         tableData = new Object[36][3];
+         tableData = new Object[34][3];
          registers = RegisterFile.getRegisters();
          for(int i=0; i< registers.length; i++){
             tableData[i][0]= registers[i].getName();
@@ -93,17 +93,9 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
          tableData[32][1]= "";//new Integer(32);
          tableData[32][2]= NumberDisplayBaseChooser.formatUnsignedInteger(RegisterFile.getProgramCounter(),valueBase);
          
-         tableData[33][0]= "hi";
-         tableData[33][1]= "";//new Integer(33);
-         tableData[33][2]= NumberDisplayBaseChooser.formatNumber(RegisterFile.getValue(33),valueBase);
-         
-         tableData[34][0]= "lo";
-         tableData[34][1]= "";//new Integer(34);
-         tableData[34][2]= NumberDisplayBaseChooser.formatNumber(RegisterFile.getValue(34),valueBase);
-
-         tableData[35][0]= "flags";
-         tableData[35][1]= "NZVC";//new Integer(34);
-         tableData[35][2]= "0000";
+         tableData[33][0]= "flags";
+         tableData[33][1]= "NZVC";//new Integer(34);
+         tableData[33][2]= "0000";
          
          return tableData;
       }
@@ -154,10 +146,8 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
             updateRegisterValue(registers[i].getNumber(), registers[i].getValue(), base);
          }
          updateRegisterUnsignedValue(32, RegisterFile.getProgramCounter(), base);
-         updateRegisterValue(33, RegisterFile.getValue(33), base);
-         updateRegisterValue(34, RegisterFile.getValue(34), base);
          // Set flag string. No function since it's unique.
-         ((RegTableModel)table.getModel()).setDisplayAndModelValueAt(RegisterFile.flagString(), 35, 2);
+         ((RegTableModel)table.getModel()).setDisplayAndModelValueAt(RegisterFile.flagString(), 33, 2);
       }
    	
      /**
